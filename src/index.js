0afe46444d75ff48ead79dbdcef5252a80264987
Henry Wu <hhw67865@gmail.com>
2022-10-13 13:12:10 -0400

    completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,68 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    createToDo(e.target.querySelector("#new-task-description").value);
+    form.reset();
+  });
+
+  function createToDo(todo) {
+
+  if (todo.length > 0) {
+    const list = document.querySelector("#list");
+    const li = document.createElement("li");
+    const btn = document.createElement("button");
+    btn.addEventListener("click", deleteToDo);
+    btn.innerText = "X";
+    li.textContent = `${todo}  `;
+    if (document.querySelector("#prioritySelect").value === 'h') {
+      li.className = 'high'
+      if (!document.querySelector(".medium")){
+
+        if (!document.querySelector(".low")) {
+        list.append(li);
+        li.appendChild(btn);
+        }
+        else {
+          list.insertBefore(li, document.querySelector(".low"))
+          li.appendChild(btn);
+        }
+      }
+      else {
+        list.insertBefore(li, document.querySelector(".medium"))
+        li.appendChild(btn);
+      }
+    }
+    if (document.querySelector("#prioritySelect").value === 'm') {
+      li.className = 'medium'
+      if (!document.querySelector(".low")) {
+        list.append(li);
+        li.appendChild(btn);
+      }
+      else {
+        list.insertBefore(li, document.querySelector(".low"))
+        li.appendChild(btn);
+      }
+    }
+    if (document.querySelector("#prioritySelect").value === 'l') {
+      li.className = 'low'
+      list.append(li);
+      li.appendChild(btn);
+    }
+    
+    
+    
+  }
+  }
+
+  function deleteToDo(e) {
+    e.target.parentNode.remove();
+  }
+
+
+
+
+
+
+
 });
